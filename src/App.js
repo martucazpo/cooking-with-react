59821350ed5f34ea14124d22cabda3e5c0289aa1
martucazpo <martucazpo@gmail.com>
2019-11-29 14:08:26 -0700

    clearing repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import React, { useState } from 'react';
-import Counter from './Counter';
-import CounterHooks from './CounterHooks'
-
-export const ThemeContext = React.createContext()
+import React from 'react';
 
 
 function App() {
-
-  const [theme, setTheme] = useState('red')
  
   return (
-    <ThemeContext.Provider value={{ backgroundColor: theme }}>
-      Counter
-      <Counter initialCount={0} />
-      Counter Hooks
-      <CounterHooks initialCount={0} />
-      <button onClick={() => setTheme(prevTheme => {
-        return prevTheme === 'red' ? 'green' : 'red'
-        })}>Toggle Theme</button>
-    </ThemeContext.Provider>
+    null
   );
 }
 
