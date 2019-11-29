f84c5b4c59d3fcb81cb07f6fd50c787f7f271fb3
martucazpo <martucazpo@gmail.com>
2019-11-29 15:01:55 -0700

    beginning JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-
+import RecipeList from './components/RecipeList'
 
 function App() {
  
   return (
-    null
-  );
+
+    <RecipeList recipes={ sampleRecipes } />
+
+  )
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions: '1. Put salt on chicken\n 2. Put chicken in oven\n 3. Eat chicken'
+  },{
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '0:45',
+    instructions: '1. Put paprika on pork\n 2. Put pork in oven\n 3. Eat pork'
+  }
+]
 
 export default App;
